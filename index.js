b5e09b53722517ca0c926c05d559e77445762a51
Isimemen Inegbedion <isimemeni@gmail.com>
2023-09-11 23:18:50 -0400

    added summary button and made good edits

--- index.js
+++ index.js
@@ -56,15 +56,22 @@ $("#data-button").on("click", function(event) {
     $(".accountability").hide();
     $(".summary").show();
 
-    $(".eval-info").append(`
-        <p>Teacher: ${$("#teacher").val()}<p>
-        <p>Grade Level: ${$("#grade").val()}<p>
-        <p>Date: ${$("#date").val()}<p>
-        <p>Observer: ${$("#observer").val()}<p>
-        <p>Content Observed: ${$("#content").val()}<p>
-        <p>Portion of the Lesson: ${$("#portion").val()}<p>
-    `)
+    // $(".eval-info").append(`
+    //     <p>Teacher: ${$("#teacher").val()}<p>
+    //     <p>Grade Level: ${$("#grade").val()}<p>
+    //     <p>Date: ${$("#date").val()}<p>
+    //     <p>Observer: ${$("#observer").val()}<p>
+    //     <p>Content Observed: ${$("#content").val()}<p>
+    //     <p>Portion of the Lesson: ${$("#portion").val()}<p>
+    // `)
 
+   
+    $("#teacher-ans").text($("#teacher").val());
+    $("#grade-ans").text($("#grade").val());
+    $("#date-ans").text($("#date").val());
+    $("#observer-ans").text( $("#observer").val());
+    $("#content-ans").text( $("#content").val());
+    $("#portion-ans").text($("#portion").val());
     $("#q1ans").text($("input[name='question1']:checked").val());
     $("#q2ans").text($("input[name='question2']:checked").val());
     $("#q3ans").text($("input[name='question3']:checked").val());
@@ -77,6 +84,24 @@ $("#data-button").on("click", function(event) {
     $("#q10ans").text($("input[name='question10']:checked").val());
     $("#q11ans").text($("input[name='question11']:checked").val());
     $("#tgaps").text($("#teacher-gaps").val());
+    $("#tglows").text($("#teacher-glows").val());
     $("#sconcerns").text($("#student-concerns").val());
-
+    $("#q1comment").text($("#question1comment").val());
+    $("#q2comment").text($("#question2comment").val());
+    $("#q3comment").text($("#question3comment").val());
+    $("#q4comment").text($("#question4comment").val());
+    $("#q5comment").text($("#question5comment").val());
+    $("#q6comment").text($("#question6comment").val());
+    $("#q7comment").text($("#question7comment").val());
+    $("#q8comment").text($("#question8comment").val());
+    $("#q9comment").text($("#question9comment").val());
+    $("#q10comment").text($("#question10comment").val());
+    $("#q11comment").text($("#question11comment").val());
   });
+
+  $("#save-button").on("click", function(event) {
+    event.preventDefault();
+
+    var element = document.getElementById('summary');
+    html2pdf(element);
+});
